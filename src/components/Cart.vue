658f5f882822b23666e3f8744b946c74869a0c71
Jeffrey Hermo <you@example.comjbhermo@yahoo.com>
2022-01-18 15:36:14 -0500

    Prototype of shoppingcart

---
+++ src/components/Cart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button v-on:click="navigateTo('products')">Back</button>
+    <h1>Add To Cart</h1>
+    <div class="products">
+      <div v-for="(product, index) in cart" :key="index">
+        {{ product.name }}
+        <img :src="product.image" />
+        <div>{{ product.cost }}</div>
+        <button v-on:click="removeItemFromCart(product)">Remove to cart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["cart"],
+  methods: {
+    removeItemFromCart(product) {
+      this.$emit("removeItemFromCart", product);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Products.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Products</h1>
+    <div class="products">
+      <div v-for="(product, index) in products" :key="index">
+        {{ product.name }}
+        <img :src="product.image" />
+        <div>{{ product.cost }}</div>
+        <button v-on:click="addItemToCart(product)">Add to cart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      products: [
+        {
+          name: "Ragnarok",
+          cost: "$0.99",
+          image: "",
+        },
+        {
+          name: "Mario",
+          cost: "$0.99",
+          image: "",
+        },
+        {
+          name: "Popeye",
+          cost: "$0.99",
+          image: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    addItemToCart(product) {
+      this.$emit("addItemToCart", product);
+    },
+  },
+};
+</script>
+
+<style></style>
