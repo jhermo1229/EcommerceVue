d49cddac1ee71980a04fcac0e0f8a15a26805e42
Jeffrey Hermo <Jhermo2967@conestogac.on.ca>
2022-02-04 17:56:17 -0500

    Fix bootstrap positioning

--- src/components/WheelsDetails.vue
+++ src/components/WheelsDetails.vue
@@ -1,22 +1,50 @@
 <template>
   <div class="d-flex wheels-details-o">
     <Header />
-    <div class="wheels-details">
-      <div class="container mb-5">
-        <h4 class="my-4">Cart</h4>
+    <div class="d-flex wheels-details">
+      <div class="container">
         <div class="row">
-          <div class="col-md-4" style="cursor: pointer">
+          <div class="col-md-2">
+            <h2 class="my-4">{{ details.name }}</h2>
+            <div>
+              <h4>Perfect for:</h4>
+              <h4>Sedan</h4>
+              <h4>Compact SUV</h4>
+            </div>
+          </div>
+          <div class="col-md-4">
             <img
               :src="details.url"
-              width="300px"
-              height="300px"
+              width="400px"
+              height="400px"
               style="border-radius: 15px"
               alt=""
             />
+
+            <div
+              v-for="items in details.comment"
+              :key="items.id"
+              class="col-md-4 pointer"
+            >
+              <h5 class="black">{{ items }}</h5>
+            </div>
+
+            <div v-for="items in cartItems" :key="items">
+              <div v-if="items.d === details.id">
+                <h5 class="black">{{ items.c }}</h5>
+              </div>
+            </div>
+            <div>
+              <input type="text" placeholder="" v-model="name" />
+              <button v-on:click="addToComment">Add comment</button>
+            </div>
           </div>
-          <div class="col-md-4" style="cursor: pointer">
-            <h5 class="my-4">Name:{{ details.name }}</h5>
-            <h5 class="my-4">Price:{{ details.price }}</h5>
+
+          <div class="col-md-4 offset-md-1" style="cursor: pointer">
+            <div class="col-md-4 offset-md-1"></div>
+            <h5 class="my-4">
+              Currently priced at: {{ details.price }} CAD per set of 4
+            </h5>
             <h5 class="my-4">Description:{{ details.desc }}</h5>
             <div>
               <button v-on:click="goToCart" class="btn btn-primary">
@@ -28,29 +56,6 @@
               <button v-on:click="removeToCart" class="btn btn-danger">
                 Remove Item
               </button>
-            </div>
-            <div class="row">
-              <div
-                v-for="items in details.comment"
-                :key="items.id"
-                class="col-md-4 pointer"
-              >
-                <h5 class="black">{{ items }}</h5>
-              </div>
-              <div>
-                <input type="text" placeholder="User Name" v-model="name" />
-                <button v-on:click="addToComment">Get input field value</button>
-              </div>
-
-              <div
-                v-for="items in cartItems"
-                :key="items"
-                class="col-md-4 pointer"
-              >
-                <div v-if="items.d === details.id">
-                  <h5 class="black">{{ items.c }}</h5>
-                </div>
-              </div>
             </div>
           </div>
         </div>
